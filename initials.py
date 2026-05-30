40b768b2c31bd4be203a9ef3733fa4b477d91bdd
saahzadkie-glitch <saahzadkie@gmail.com>
2026-05-30 23:52:33 +0000

    Add files via upload

--- initials.py
+++ initials.py
@@ -1,7 +1,35 @@
+# File: initials.py
+# Description: Prints my initials in large stylized letters.
+# Assignment Number: 1
+#
+# Name: Zadkiel Saah
+# STUDENT ID: 2425402868
+# Email: Saahzadkie@gmail.com
+# Grader: Carolyn
+#
+# On my honor, Zadkiel Saah, this programming assignment is my own work
+# and I have not provided this code to any other student.
+
 
 def main():
-  pass #code goes here
+    # Prints my initials in normal and large format
 
+    print()
+    print("...ZYS")
+    print()
+
+    print("...ZZZZZZZZZZZZ...****...YY........YY...****...SSSSSSSSSSSS")
+    print("...........ZZZZ...****....YY......YY....****...SSSSSSSSSSSS")
+    print("..........ZZZZ....****.....YY....YY.....****...SS..........")
+    print(".........ZZZZ.....****......YY..YY......****...SS..........")
+    print("........ZZZZ......****.......YYYY.......****...SSSSSSSS....")
+    print(".......ZZZZ.......****........YY........****...SSSSSSSSSSSS")
+    print("......ZZZZ........****........YY........****...........SSSS")
+    print(".....ZZZZ.........****........YY........****.............SS")
+    print("....ZZZZ..........****........YY........****...SSSSSSSSSSSS")
+    print("...ZZZZZZZZZZZZ...****........YY........****...SSSSSSSSSSSS")
+
+    print()
 
 
 main()
